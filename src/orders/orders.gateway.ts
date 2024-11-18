7a1b57ce5cada755f66b5ed67982ca286a599c53
obkin <kapellwork@gmail.com>
2024-11-18 17:26:52 +0200

    error: retCode: 10001

--- src/orders/orders.gateway.ts
+++ src/orders/orders.gateway.ts
@@ -29,10 +29,14 @@ export class OrdersGateway
 
   handleDisconnect() {
     this.logger.log('Client disconnected from WebSocket');
+    if (this.ws) {
+      this.ws.close();
+    }
+    setTimeout(() => this.connectToBybit(), 5000);
   }
 
   private connectToBybit() {
-    const url = 'wss://stream.bybit.com/v5/private'; // correct url??
+    const url = 'wss://stream-testnet.bybit.com/v5/trade'; // correct url??
     this.ws = new WebSocket(url);
 
     this.ws.on('open', () => {
@@ -53,12 +57,21 @@ export class OrdersGateway
   }
 
   private authenticate() {
-    const apiKey = this.configService.get<string>('API_KEY');
-    const apiSecret = this.configService.get<string>('API_SECRET');
-    const timestamp = Date.now().toString();
+    const apiKey = this.configService.get<string>('BYBIT_API_KEY');
+    const apiSecret = this.configService.get<string>('BYBIT_API_SECRET');
+    const expires = Date.now() + 60 * 1000;
+    const apiTimestamp = Math.floor(expires / 1000).toString();
 
-    const signatureString = `api_key=${apiKey}&timestamp=${timestamp}`;
+    if (!apiKey) {
+      this.logger.error('BYBIT_API_KEY not found');
+      return;
+    }
+    if (!apiSecret) {
+      this.logger.error('BYBIT_API_SECRET not found');
+      return;
+    }
 
+    const signatureString = `api_key=${apiKey}&api_timestamp=${apiTimestamp}`;
     const signature = crypto
       .createHmac('sha256', apiSecret)
       .update(signatureString)
@@ -66,8 +79,10 @@ export class OrdersGateway
 
     const authMessage = {
       op: 'auth',
-      args: [apiKey, signature, timestamp],
+      args: [apiKey, apiTimestamp, signature],
     };
+
     this.ws.send(JSON.stringify(authMessage));
+    this.logger.log('Authentication message sent');
   }
 }
